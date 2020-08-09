dc9b4f4d6cc292384ac50343aef60101668341f7
Bibek Koirala <bibek.high@gmail.com>
2020-08-09 15:52:28 +0530

    Longest Increasing Subsequence Problem

--- Longest_Increasing_Subsequence.py
+++ Longest_Increasing_Subsequence.py
@@ -9,6 +9,7 @@ def Longest_Increasing_Subsequence():
     i = j = 0
     for i in range(length):
         for j in range(i+1):
+            print(LIS)
             if arr[i] > arr[j] and LIS[i] < LIS[j]+1:
                 LIS[i] = LIS[j] + 1
 
