63688e8c96f4a4f17fcbce2140faea19f9165860
Bibek Koirala <bibek.high@gmail.com>
2020-08-09 17:05:54 +0530

    Binomial Coefficient Problem Using Recursive and Dynamic Programming Approach

---
+++ Binomial_Coefficient.py
@@ -0,0 +1,30 @@
+
+# Recursive non DP approach
+def BCP(n,k):
+    if k==0 or n<=k:
+        return 1
+    else:
+        return BCP(n-1,k) + BCP(n-1,k-1)
+
+print(BCP(4,2))
+
+# Dynamic Programming Approach
+
+def DynamicBCP(n,k):
+    DPA = [[0 for i in range(n+1)] for j in range(k+1)]
+    i = 0
+    while i <= k:
+        j = 0
+        while j <= n:
+            if j == i or i == 0:
+                DPA[i][j] = 1
+            elif j < i:
+                DPA[i][j] = 0
+            else:
+                DPA[i][j] = DPA[i][j-1] + DPA[i-1][j-1]
+            j += 1
+        i += 1
+
+    print(DPA[i-1][j-1])
+
+DynamicBCP(4,2)
